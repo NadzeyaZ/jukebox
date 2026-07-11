838a438ba36c3c105ba410f63657fa84521b8ab9
Nadzeya Zianiuk <nadzeya@nadzeyas-air.mynetworksettings.com>
2026-07-11 11:06:00 -0400

    Server - playlists

--- api/playlists.js
+++ api/playlists.js
@@ -1,8 +1,11 @@
 import {
   createPlaylist,
+  getAllTracksInPlaylist,
   getPlaylistById,
   getPlaylists,
 } from "#db/queries/playlists";
+import { getTrackById } from "#db/queries/tracks";
+import { createPlaylistsTracksRow } from "#db/queries/playlists_tracks";
 import express from "express";
 const router = express.Router();
 export default router;
@@ -24,8 +27,47 @@ router.post("/", async (req, res) => {
 });
 
 router.param("id", async (req, res, next, id) => {
+  if (isNaN(Number(id))) {
+    return res.status(400).send();
+  }
   const playlist = await getPlaylistById(id);
   if (!playlist) return res.status(404).send("Playlist not found");
   req.playlist = playlist;
   next();
+});
+
+router.get("/:id", async (req, res) => {
+  const playlist = await getPlaylistById(req.playlist.id);
+  res.send(playlist);
+});
+
+router.get("/:id/tracks", async (req, res) => {
+  const playlist = await getAllTracksInPlaylist(req.playlist.id);
+  res.send(playlist);
+});
+
+router.post("/:id/tracks", async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send();
+  }
+  const { trackId } = req.body;
+  if (!trackId) {
+    return res.status(400).send();
+  }
+  if (isNaN(Number(trackId))) {
+    return res.status(400).send();
+  }
+  const track = await getTrackById(trackId);
+  if (!track) {
+    return res.status(400).send();
+  }
+  const tracks = await getAllTracksInPlaylist(req.playlist.id);
+  if (tracks.find((track) => track.id === trackId)) {
+    return res.status(400).send("Track is already in playlist");
+  }
+  const playlist_track = await createPlaylistsTracksRow(
+    req.playlist.id,
+    trackId,
+  );
+  res.status(201).send(playlist_track);
 });
